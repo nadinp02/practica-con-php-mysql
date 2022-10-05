6321dc20d31b2ca6115e0eb8f81d5db6f20efa52
nadinp02 <pereznadin6@gmail.com>
2022-10-05 13:01:42 -0300

    05-10

--- servicio.php
+++ servicio.php
@@ -11,39 +11,34 @@ $content = $contents->getById($id);
 $image = $images->getByCod($content['cod']);
 
 ?>
-<h2 class="mt-4 text-center"><?= $content['title'] ?></h2>
-<div class=" mt-4 text-center"><?= $content['content'] ?></div>
 
-<div id="intro" class="carousel slide pb-5" data-bs-ride="carousel">
+<div class="container container-img-carousel">
+    <div id="slider" class="carousel carousel-dark slide" data-bs-ride="carousel">
 
-    <div class="carousel-indicators pb-5">
+        <div class="carousel-indicators pb-0">
+            <?php foreach ($image as $key => $img) { ?>
+                <button data-bs-target="#slider" data-bs-slide-to="<?= $key ?>" class="<?= $key == 0 ? 'active' : '' ?>"></button>
+            <?php } ?>
+        </div>
+        <div class="carousel-inner">
+            <?php foreach ($image as $key => $img) { ?>
+                <div class="carousel-item <?= $key == 0 ? 'active' : '' ?>">
+                    <img class="img-carousel" src="<?= URL ?>admin/<?= $img['url'] ?>">
+                </div>
+                <div class="carousel-caption d-none d-md-block">
+                    <h5><?= $content['title'] ?></h5>
+                    <p><?= $content['content'] ?></p>
+                </div>
+            <?php } ?>
+        </div>
+        <button class="carousel-control-prev" data-bs-target="#slider" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </button>
 
-        <?php foreach ($image as $key => $img) { ?>
-            <button data-bs-target="#intro" data-bs-slide-to="<?= $key ?>" class="<?= $key == 0 ? 'active' : '' ?>"></button>
-        <?php } ?>
+        <button class="carousel-control-next" data-bs-target="#slider" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </button>
     </div>
-
-
-    <div class="carousel-inner">
-
-        <?php foreach ($image as $key => $img) {
-        ?>
-            <div class="carousel-item <?= $key == 0 ? 'active' : '' ?>">
-                <img class="mt-4 mx-auto d-block" src="<?= URL ?>admin/<?= $img['url'] ?>">
-            </div>
-        <?php
-        }
-        ?>
-    </div>
-
-    <button class="carousel-control-prev" data-bs-target="#intro" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon"></span>
-    </button>
-
-    <button class="carousel-control-next" data-bs-target="#intro" data-bs-slide="next">
-        <span class="carousel-control-next-icon"></span>
-    </button>
-
 </div>
 
 <?php
